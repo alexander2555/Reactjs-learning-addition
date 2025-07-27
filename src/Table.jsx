664613159b8d0e07ed04a9b6282498b60b565888
Alexander Mamontov <alexander2555@yandex.ru>
2025-07-27 10:05:42 +0200

    Initial

---
+++ src/Table.jsx
@@ -0,0 +1,14 @@
+import { data } from './data' //  используем начальные данные
+
+import './Table.css'
+
+function Table() {
+	return (
+		<>
+			<h1>Table</h1>
+			{/* делаем динамический рендер таблицы */}
+		</>
+	)
+}
+
+export default Table
